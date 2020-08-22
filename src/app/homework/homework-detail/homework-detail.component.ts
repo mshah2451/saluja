c3063b2d8f67d3e3f8c072ea817da1c3c6e81a98
ankit.kashyap <ankit.kashyap@quovantis.com>
2020-08-22 23:38:37 +0530

    :beetle: add fix for known issue

--- src/app/homework/homework-detail/homework-detail.component.ts
+++ src/app/homework/homework-detail/homework-detail.component.ts
@@ -5,6 +5,7 @@ import {HomeworkService} from '../homework-service';
 import {FileTransfer, FileTransferObject,FileUploadOptions} from '@ionic-native/file-transfer/ngx'
 import { File } from '@ionic-native/file/ngx';
 import { AuthService } from 'src/app/auth/auth.service';
+//import {FileDowmloaderService} from '../../services/file.dowmloader.service'
 
 @Component({
   selector: 'app-homework-detail',
@@ -18,7 +19,9 @@ export class HomeworkDetailComponent implements OnInit {
     private homeworkService:HomeworkService,
     private transfer: FileTransfer, 
     private file: File,
-    private authService:AuthService,) 
+    private authService:AuthService,
+  //  private fileDowmloaderService:FileDowmloaderService
+    ) 
   {}
 
   ngOnInit() {
@@ -28,29 +31,31 @@ export class HomeworkDetailComponent implements OnInit {
   private fileTransfer: FileTransferObject; 
   
 
-  public download() {  
-    const fileName="myfile";
-    let token='';
-    this.authService.token.subscribe(val=>token=val);
-    const options: FileUploadOptions = {
-      fileKey: 'file',
-      headers: {
-        "Authorization":"Bearer "+`${token}`
-      }
-    };
-      //here encoding path as encodeURI() format.  
-      let url = encodeURI(`https://app.salujagoldschool.com/UploadHomework/9932d775-eb22-43da-8bb9-66f48ba41714.jpg`);  
-      //here initializing object.  
-      this.fileTransfer = this.transfer.create();  
-      // here iam mentioned this line this.file.externalRootDirectory is a native pre-defined file path storage. You can change a file path whatever pre-defined method.  
-      this.fileTransfer.download(url, this.file.externalRootDirectory + fileName, true,options).then((entry) => {  
-          //here logging our success downloaded file path in mobile.  
-          console.log('download completed: ' + entry.toURL());  
-      }, (error) => {  
-          //here logging our error its easier to find out what type of error occured.  
-          console.log('download failed: ' + error);  
-      });  
+  public download(url:string) {  
+   //this.fileDowmloaderService.downloadFile(url,'myFile')
   }
+
+   // const fileName="myfile";
+    // let token='';
+    // this.authService.token.subscribe(val=>token=val);
+    // const options: FileUploadOptions = {
+    //   fileKey: 'file',
+    //   headers: {
+    //     "Authorization":"Bearer "+`${token}`
+    //   }
+    // };
+    //   //here encoding path as encodeURI() format.  
+    //   let url = encodeURI(`https://app.salujagoldschool.com/UploadHomework/9932d775-eb22-43da-8bb9-66f48ba41714.jpg`);  
+    //   //here initializing object.  
+    //   this.fileTransfer = this.transfer.create();  
+    //   // here iam mentioned this line this.file.externalRootDirectory is a native pre-defined file path storage. You can change a file path whatever pre-defined method.  
+    //   this.fileTransfer.download(url, this.file.externalRootDirectory + fileName, true,options).then((entry) => {  
+    //       //here logging our success downloaded file path in mobile.  
+    //       console.log('download completed: ' + entry.toURL());  
+    //   }, (error) => {  
+    //       //here logging our error its easier to find out what type of error occured.  
+    //       console.log('download failed: ' + error);  
+    //   });  
   
 
   onCancel() {
