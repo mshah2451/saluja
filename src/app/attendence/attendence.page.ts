cc0873bb94d13ad69f5407b57b1675f38fa5c535
ankit.kashyap <ankit.kashyap@quovantis.com>
2020-09-01 21:16:51 +0530

    update

--- src/app/attendence/attendence.page.ts
+++ src/app/attendence/attendence.page.ts
@@ -1,48 +1,88 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import {AttendanceService} from '../services/attendance.service';
 import {LoaderService} from '../services/loader.service';
+import { CalendarComponent } from 'ionic2-calendar';
 @Component({
   selector: 'app-attendence',
   templateUrl: './attendence.page.html',
   styleUrls: ['./attendence.page.scss'],
 })
 export class AttendencePage implements OnInit {
+  @ViewChild(CalendarComponent) myCalendar:CalendarComponent;
 
   constructor(private attendanceService:AttendanceService
-    ,private loaderService:LoaderService) { }
+    , private loaderService: LoaderService)
+  {
+    
+  }
   currentDate:Date;
   calendar={
     mode:'month',
-    currentDate : this.currentDate
+    currentDate: this.currentDate,
+    queryMode:'remote'
   };
   totalPresent:number=0;
   totalAbsent:number=0;
   weekDayName:string="";
   monthName:string="";
-  yearName:string="";
+  yearName: string = "";
+  eventSource = [];
 
   ngOnInit() {
-   const currentDate =new Date();
-   this.updateStudentAttendence(currentDate);
+    const currentDate =new Date();
+    this.updateStudentAttendence(currentDate);
+    // this.addEvent({
+    //   DATE: new Date().toUTCString(),
+    //   PRESENT_STATUS: "P"
+    // });
+  }
+
+  addEvent(element: any) { 
+    const date = new Date(element.DATE);
+    const event = {
+      title: 'Event',
+      startTime: date,
+      endTime: date,
+      allDay: false,
+      color:this.getStatusColor(element.PRESENT_STATUS)
+    };
+    return event;
+  }
+
+  getStatusColor(status: any) { 
+    if (status === 'P') {
+      return 'present';
+    }
+    else if (status === 'A') { 
+      return 'absent';
+    }
+    return 'holiday';
   }
 
   updateStudentAttendence(date:Date){
-    try{
-    this.loaderService.showLoader();
+    try {
+     
+  //this.loaderService.showLoader();
     const month = date.getMonth() + 1;
     const year = date.getFullYear();
     this.attendanceService.getAttendaceByStudentId(month,year).subscribe(attendence=>{
       this.totalPresent = attendence[0][0].Total_Present;
       this.totalAbsent = attendence[0][0].Total_Absent
       this.updateDateObject(date);
-      this.loaderService.hideLoader();
+      this.eventSource.splice(0, this.eventSource.length);
+     const events =  attendence[0].map(element => {
+      return  this.addEvent(element);
+     });
+      this.eventSource = [...events];
+     this.myCalendar.loadEvents();
+      //this.loaderService.hideLoader();
       });
     }
     finally{
-      this.loaderService.hideLoader();
+    //this.loaderService.hideLoader();
     }
   }
-
+  
   updateDateObject(date:Date){
     this.weekDayName=this.getWeekDayName(date);
     this.monthName=this.getMonthName(date);
